17658d27f2580cfb0c822df58cbd4784ebe1633a
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-08-25 16:31:35 -0400

    feat: add banner with total invoicing and new record button on supplies page

--- src/routes/supplies/+page.svelte
+++ src/routes/supplies/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import IconCalendar from '~icons/mdi/Calendar';
+
 	export let data;
 
 	function formatCurrency(value: number) {
@@ -11,82 +13,75 @@
 </svelte:head>
 
 <div class=" sm:ml-64">
-	<div class="mt-[3.5rem] p-4">
-		<h2 class="text-3xl font-extrabold text-primary strong-green mb-4">Estoque</h2>
+	<div class="mt-[3.5rem] p-4 flex flex-col gap-4">
+		<h2 class="text-3xl font-extrabold text-primary strong-green">Estoque</h2>
 
-		<div class="overflow-x-auto mb-8">
-			<table class="table">
-				<thead>
-					<tr>
-						<th></th>
+		<div
+			class="flex justify-between rounded-lg p-4 text-white font-bold"
+			style="background-color: #00a868;"
+		>
+			<div>
+				<h3>
+					Faturamento total: {formatCurrency(5950)}
+				</h3>
+			</div>
 
-						<th> Qtd. de itens </th>
-
-						<th> Valor de compra </th>
-
-						<th> Faturamento </th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th> Total </th>
-
-						<td> 4000 </td>
-
-						<td>
-							{formatCurrency(3678)}
-						</td>
-
-						<td>
-							{formatCurrency(5950)}
-						</td>
-					</tr>
-				</tbody>
-			</table>
+			<div class="flex items-center gap-1">
+				<IconCalendar />
+				<span> 24 ago 2024 </span>
+			</div>
 		</div>
 
-		<div class="overflow-x-auto">
-			<table class="table table-zebra">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Item</th>
-						<th>Quantidade</th>
-						<th>Valor de compra</th>
-						<th>Valor de venda</th>
-						<th>Faturamento</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each data.items as item}
+		<div class="flex flex-col gap-2">
+			<div class="flex justify-between">
+				<h3 class="text-xl font-bold text-primary strong-green">Itens</h3>
+
+				<button class="btn btn-sm"> + Novo registro </button>
+			</div>
+
+			<div class="overflow-x-auto">
+				<table class="table table-zebra">
+					<thead>
 						<tr>
-							<th>
-								{item.id}
-							</th>
-
-							<td>
-								{item.name}
-							</td>
-
-							<td>
-								{item.quantity}
-							</td>
-
-							<td>
-								{formatCurrency(item.purchasePrice)}
-							</td>
-
-							<td>
-								{formatCurrency(item.salePrice)}
-							</td>
-
-							<td>
-								{formatCurrency(item.invoicing)}
-							</td>
+							<th>#</th>
+							<th>Item</th>
+							<th>Quantidade</th>
+							<th>Valor de compra</th>
+							<th>Valor de venda</th>
+							<th>Faturamento</th>
 						</tr>
-					{/each}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{#each data.items as item}
+							<tr>
+								<th>
+									{item.id}
+								</th>
+
+								<td>
+									{item.name}
+								</td>
+
+								<td>
+									{item.quantity}
+								</td>
+
+								<td>
+									{formatCurrency(item.purchasePrice)}
+								</td>
+
+								<td>
+									{formatCurrency(item.salePrice)}
+								</td>
+
+								<td>
+									{formatCurrency(item.invoicing)}
+								</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 </div>
