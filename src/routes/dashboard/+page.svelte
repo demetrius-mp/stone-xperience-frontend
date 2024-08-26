700199eec3a7151df0ad5b20de04465a40a1b964
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-08-25 20:58:33 -0400

    feat: add intervals and export button

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Chart from '$lib/components/Chart.svelte';
+	import IconExportVariant from '~icons/mdi/ExportVariant';
 
 	export let data;
 
@@ -12,11 +13,50 @@
 	<title>StoneXperience - Dashboard</title>
 </svelte:head>
 
-<div class=" sm:ml-64">
+<div class="sm:ml-64">
 	<div class="mt-[4.5rem] p-4">
-		<h2 class="text-3xl font-extrabold text-primary strong-green mb-4">Dashboard</h2>
+		<div class="flex gap-4 items-center justify-between">
+			<div class="flex gap-4 items-center">
+				<h2 class="text-3xl font-extrabold text-primary strong-green">Dashboard</h2>
 
-		<div class="flex flex-col gap-4 xl:grid xl:grid-cols-3">
+				<div class="join">
+					<button
+						class="btn btn-sm btn-primary border-white border-e join-item bg-dashboard-chart text-white"
+					>
+						Diário
+					</button>
+					<button
+						class="btn btn-sm btn-primary border-white border-e join-item bg-dashboard-chart text-white"
+					>
+						Semanal
+					</button>
+					<button
+						class="btn btn-sm btn-primary border-white border-e join-item bg-dashboard-chart text-white"
+					>
+						Mensal
+					</button>
+					<button
+						class="btn btn-sm btn-primary border-white border-e join-item bg-dashboard-chart text-white"
+					>
+						Semestral
+					</button>
+					<button
+						class="btn btn-sm btn-primary border-white border-e join-item bg-[#00a465] text-white"
+					>
+						Anual
+					</button>
+				</div>
+			</div>
+
+			<button
+				class="btn btn-sm border-white border-e btn-primary bg-dashboard-chart text-item text-white"
+			>
+				<IconExportVariant />
+				Exportar
+			</button>
+		</div>
+
+		<div class="flex flex-col gap-4 xl:grid xl:grid-cols-3 mt-4">
 			<div class="p-4 bg-dashboard-chart rounded-xl col-span-1">
 				<Chart config={data.datasets.metaDeFaturamento} />
 
